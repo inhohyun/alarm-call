6906a39da3e25196f4785479d56956d0e7d29ef3
MoonSheep08 <gus5tmd24@naver.com>
2022-10-11 22:47:20 +0900

    주석 달아놓음

--- app/src/main/java/com/shoppi/alarm/Maintest_Activity.java
+++ app/src/main/java/com/shoppi/alarm/Maintest_Activity.java
@@ -12,6 +12,8 @@ public class Maintest_Activity extends AppCompatActivity {
     Button set_button;
 
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+//        기존 코드, 상속받은 AppCompatActivity이외의 코드도 동작함을 의미
+//        이 코드를 생략하면 이후로 작성된 코드만 동작함을 의미
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_test);
         set_button=   (Button)findViewById(R.id.setting_button);
